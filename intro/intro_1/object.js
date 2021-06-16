0b1ed9746c91264eabf6236b6ea7b10e90d1388c
librankn <kkuroto@gmai.com>
2021-06-16 23:10:39 +0900

    6月16日作業分

--- intro/intro_1/object.js
+++ intro/intro_1/object.js
@@ -1,16 +1,18 @@
-let startTime = null;
-let displayArea = document.getElementById('display-area');
+let game = {
+  startTime: null,
+  displayArea: document.getElementById('display-area'),
+};
 function start() {
-  startTime = Date.now();
+  game.startTime = Date.now();
   document.body.onkeydown = stop;
 }
 function stop() {
   let currentTime = Date.now();
-  let seconds = (currentTime - startTime) / 1000;
+  let seconds = (currentTime - game.startTime) / 1000;
   if (9.5 <= seconds && seconds <= 10.5) {
-    displayArea.innerText = seconds + '秒でした。すごい。';
+    game.displayArea.innerText = seconds + '秒でした。すごい。';
   } else {
-    displayArea.innerText = seconds + '秒でした。残念。';
+    game.displayArea.innerText = seconds + '秒でした。残念。';
   }
 }
 if (confirm('OKを押して10秒だと思ったら何かキーを押して下さい')) {
